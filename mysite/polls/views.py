57cf4d1aaa5a4af1aa997258100fc810e64166aa
gittykite <kitelink4@gmail.com>
2020-11-29 11:58:41 +0900

    polls: views & urls added

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,19 @@
+# WHEN CHANGED:
+# add urls of the views at ./urls.py
+# https://docs.djangoproject.com/ko/3.1/intro/tutorial03/#writing-more-views
 from django.http import HttpResponse
 
 
 def index(request):
     return HttpResponse("You're at the polls index.")
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s" % question_id)
+
+def result(request, question_id):
+    response = "The result of question %s"
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("Voting on question %s" % question_id)
+
