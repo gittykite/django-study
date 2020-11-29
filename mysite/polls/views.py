1d99c29bcefb46d50ba7cefbbfdab0c0572a918b
gittykite <kitelink4@gmail.com>
2020-11-29 14:52:33 +0900

    polls: list questions at index (hard-coding)

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,9 +3,16 @@
 # https://docs.djangoproject.com/ko/3.1/intro/tutorial03/#writing-more-views
 from django.http import HttpResponse
 
+from .models import Question
+
 
 def index(request):
-    return HttpResponse("You're at the polls index.")
+    # get 5 latest questions from DB 
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+
+    # display question list (ver.hard-coding)
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s" % question_id)
